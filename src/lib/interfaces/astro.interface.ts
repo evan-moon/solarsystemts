d76f8557748c7df42091f783dac2e80600f669f4
Evan Moon <bboydart91@gmail.com>
2018-01-06 19:52:40 +0900

    Add ComputedOrbitData to astro.interface

--- src/lib/interfaces/astro.interface.ts
+++ src/lib/interfaces/astro.interface.ts
@@ -1,4 +1,4 @@
-import { Object3D, Color } from 'three';
+import { Object3D, Vector3, Color } from 'three';
 
 export interface Material {
     emissive?: Color;
@@ -22,6 +22,32 @@ export interface OrbitData {
     cy?: Kepler6Elements;
     day?: Kepler6Elements;
 };
+
+export interface ComputedOrbitData {
+    time: Date; // 해당 데이터에 대한 시간
+    a: number; // Semi-majot axis (장반경)
+    e: number; // Eccentricity (이심률)
+    i: number; // Inclination (궤도 기울기)
+    o: number; // Longitude of Ascending Node (Ω)
+    w: number; // Argument of periapsis (ω)
+
+    E: number; // Eccentric Anomaly
+    T: number; // Time at perihelion
+    M: number; // Mean anomaly
+
+    l: number; // Mean longitude
+    lp: number; // Longitude of periapsis
+
+    r: number; // Distance from center
+    v: number; // True anomaly (through Kepler)
+
+    P: number; // Sidereal period
+    Pw: number; // Argument of periapsis precession period (mean value)
+    Pn: number; // Longitude of the ascending node precession period (mean value)
+
+    pos: Vector3;
+    tilt?: number;
+}
 
 export interface AtmoSphereComponent {
     id: string;
@@ -50,7 +76,7 @@ export interface AstronomicalObjectData {
 }
 
 export interface PlanetData extends AstronomicalObjectData {
-    orbit?: OrbitData;
+    orbit: OrbitData;
     tilt?: number;
     atmosphere?: AtmoSphere;
     ring?: Ring;
